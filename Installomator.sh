4b71ccde7696f11a04ec3e7085f1e1449101b3e5
mtbhuskies <60019431+mtbhuskies@users.noreply.github.com>
2024-11-13 10:04:41 -0800

    Update Installomator.sh

--- Installomator.sh
+++ Installomator.sh
@@ -1592,6 +1592,9 @@ sketchup2024)
 zalo)
     name="Zalo"
     type="dmg"
+    # Set the NODE_PATH to include global modules
+    export NODE_PATH=$(npm root -g)
+    
     # Path to the Node.js script that retrieves the latest download URL
     nodeScript="/Library/Application Support/Ruffwear/zalogetversion.js"
     
